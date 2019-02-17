91945884b7b33a145c49af50bad49766d3412330
Vasil Dimitrov <vasil@twentythree.net>
2019-02-17 21:03:30 +0100

    Add slider. Add scroll to contact form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,8 +3,7 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
-
-window.onload = function() {
+function handleForm() {
     var inputs = document.querySelectorAll('.js-input');
     var submitButton = document.querySelector('#js-submit-button');
 
@@ -43,4 +42,53 @@ window.onload = function() {
             }
         };
     });
+}
+
+function handleCarousel() {
+    var carousels = document.querySelectorAll('.js-carousel');
+
+    carousels.forEach(function(carousel) {
+        new Glider(carousel.querySelector('ul'), {
+            slidesToShow: 1,
+            arrows: {
+                prev: carousel.querySelector('.arrow-left'),
+                next: carousel.querySelector('.arrow-right')
+              }
+        });
+    });
+}
+
+function handleScrollToForm() {
+    var body = document.scrollingElement || document.documentElement;
+    var items = document.querySelectorAll('.js-scroll-to-form');
+    var form = document.querySelector('#contact-me');
+    var time = 1000;
+
+    items.forEach(function(item) {
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            var from = body.scrollTop;
+            var offset = form.getBoundingClientRect().top;
+            var start = new Date().getTime();
+
+            window.requestAnimationFrame(scrollAnimation);
+
+            function scrollAnimation() {
+                var step = Math.min(1, (new Date().getTime() - start) / time);
+                body.scrollTop = from + (step * offset);
+
+                if (step !== 1) {
+                    window.requestAnimationFrame(scrollAnimation);
+                }
+            }
+
+        });
+    });
+}
+
+
+window.onload = function() {
+    handleForm();
+    handleCarousel();
+    handleScrollToForm();
 };
