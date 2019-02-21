cffe0b33ca7d2022a9a58f789ede55e880d52f8a
Vasil Dimitrov <vasil@twentythree.net>
2019-02-21 22:02:19 +0100

    Php

--- api/send-mail.php
+++ api/send-mail.php
@@ -1,11 +1,21 @@
 <?php
-    if ( isset( $_POST['name'])) {
-        $name = strip_tags(trim($_POST['name'] ) );
+    if (isset( $_POST['name'])) {
+        $name = strip_tags(trim($_POST['name']));
     }
-    if ( isset( $_POST['email'])) {
-        $email = strip_tags(trim($_POST['email'] ) );
+
+    if (isset( $_POST['email'])) {
+        $email = strip_tags(trim($_POST['email']));
     }
-    
+
+    if (isset( $_POST['subject'])) {
+        $subject = strip_tags(trim($_POST['subject']));
+    }
+
+    if (isset( $_POST['details'])) {
+        $details = strip_tags(trim($_POST['details']));
+    }
+
     echo $name;
     echo $email;
+    echo $boza;
 ?>
