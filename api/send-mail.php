fd9b5eb57692b18106dfeb4bc168e2e77c44ba5a
Vasil Dimitrov <vasil@twentythree.net>
2019-02-21 21:59:13 +0100

    Play with php

--- api/send-mail.php
+++ api/send-mail.php
@@ -1,6 +1,11 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
+    if ( isset( $_POST['name'])) {
+        $name = strip_tags(trim($_POST['name'] ) );
+    }
+    if ( isset( $_POST['email'])) {
+        $email = strip_tags(trim($_POST['email'] ) );
+    }
+    
     echo $name;
     echo $email;
 ?>
