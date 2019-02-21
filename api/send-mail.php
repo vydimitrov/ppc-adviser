3d94badeb56c7b0bdebbb9b8d109635355451b76
Vasil Dimitrov <vasil@twentythree.net>
2019-02-21 22:21:02 +0100

    Form

--- api/send-mail.php
+++ api/send-mail.php
@@ -15,9 +15,14 @@
         $message = strip_tags(trim($_POST['message']));
     }
 
+    $freeAudit = "no";
+    if (isset($_POST['free-audit']) and $_POST['free-audit'] == "on") {
+        $freeAudit = "yes";
+    }
+
     $recipient = "boryana.dimitrova@outlook.com";
     $mailheader = "From: $email \r\n";
-    $formcontent = "Name: $name \nEmail: $email \nSubject: $subject \nMessage: $message";
+    $formcontent = "Name: $name \nEmail: $email \nSubject: $subject \nMessage: $message\n Free Audit: $freeAudit";
     $emailSubject = 'New contact form submition';
 
     mail($recipient, $emailSubject, $formcontent, $mailheader) or die("Error!");
